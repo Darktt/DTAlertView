d1dd6dbe00dee704c2a02e0ba03c5aaabbd23f23
darktt <pippipig@gmail.com>
2013-10-08 16:25:33 +0800

    Add document set.

---
+++ Docset/GeneratorDocset.sh
@@ -0,0 +1,1 @@
+appledoc --output ~/Desktop/ --project-name "DTAlertView" --project-company "Darktt personal company" --company-id "com.darktt" --no-warn-undocumented-object --keep-intermediate-files ../DTAlertView/
